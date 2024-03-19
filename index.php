500d858f50c5e47eb571ba5cdc25b376a7fd5de6
kiranpjc7@gmail.com <kiranpjc7@gmail.com>
2024-03-19 20:25:32 +0530

    sadcfd

--- index.php
+++ index.php
@@ -303,7 +303,7 @@
         for ($i = 0; $i < count($courses_array); $i++) {
         ?>
          
-            <div class="col-md-3">
+            <div class="col-md-2">
               <a href="uni_list.php" class="h-100">
                 <div class="mb-3 rounded-5">
                   <div class="bg-image" style="max-width: 262px !important; position:relative; margin: 0px auto; border-radius: 10px !important">
