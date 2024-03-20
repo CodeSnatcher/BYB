f9d5074a847da0f78d8c7f4fbecb051c3765edaa
kiranpjc7@gmail.com <kiranpjc7@gmail.com>
2024-03-20 16:03:12 +0530

    svsvsv

--- agent/resources/views/program.blade.php
+++ agent/resources/views/program.blade.php
@@ -472,7 +472,7 @@
 
                                                             <div class="card p-2 rounded-3 mb-3 border">
                                                                 <div class="d-flex gap-3  ">
-                                                                    <input type="radio" name="stu_id" onchange="student({{$studata->id}})" id="stu" value="{{$studata->id}}" class="form-check-input
+                                                                    <input type="radio" name="stu_id" onchange="student({{$studata->id}})" id="stu{{$studata->id}}" value="{{$studata->id}}" class="form-check-input
                                                                     
                                                                     " />
                                                                     <div class="text-dark fs-5 text-primary fw-bold">#{{$studata->id}}</div>
@@ -734,7 +734,7 @@
 
     <script>
         function student(stu_id) {
-            alert($(`#stu`).val());
+            alert($(`#stu${stu_id}`).val());
 
             $.ajax({
                 type: "POST",
